4919d28eea5783736d9fb9b6f87faefa188a6d7f
Ayuba Olanrewaju <Babarindeolanrewaju@gmail.com>
2020-12-12 08:47:53 +0000

    fetched the data and install material ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gallery: [],
+      gallery2: [],
+    };
+
+  }
+
+  async componentDidMount() {
+    try {
+      //fetching data from the Api
+      const response = await fetch(`https://jsonplaceholder.typicode.com/photos`);
+      const json = await response.json();
+
+      //filtering the data for even items
+      const evensArray = json.filter(item => item.id % 2 === 0);
+      console.log("evens-data",evensArray)
+
+      //splicing the data into 50
+      this.setState({ gallery: evensArray.splice(0, 50)  });
+      this.setState({ gallery2: this.state.gallery.splice(0, 10) });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <div> textInComponent </div>
+    );
+  }
 }
 
 export default App;
